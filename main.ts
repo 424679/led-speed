0e0773a8bb07cacb9dac8a5c1c9c3dcaa3e24598
424679 <91246978+424679@users.noreply.github.com>
2021-12-16 16:46:14 +0800

    增加A"B"B

--- main.ts
+++ main.ts
@@ -1,16 +1,39 @@
 function led2 (y: number, speed: number) {
     while (true) {
-        for (let index = 0; index <= 4; index++) {
-            if (list[y][index] == 1) {
-                led.plot(index, y)
-            } else {
-                led.unplot(index, y)
+        if (input.buttonIsPressed(Button.A)) {
+            while (true) {
+                for (let index = 0; index <= 4; index++) {
+                    if (list[y][index] == 1) {
+                        led.plot(index, y)
+                    } else {
+                        led.unplot(index, y)
+                    }
+                }
+                list[y].unshift(list[y].pop())
+                basic.pause(speed)
             }
         }
-        list[y].unshift(list[y].pop())
-        basic.pause(speed)
+        if (input.buttonIsPressed(Button.B)) {
+            while (true) {
+                for (let index = 0; index <= 4; index++) {
+                    if (list[y][index] == 1) {
+                        led.plot(y, index)
+                    } else {
+                        led.unplot(y, index)
+                    }
+                }
+                list[y].unshift(list[y].pop())
+                basic.pause(speed)
+            }
+        }
     }
 }
+input.onButtonPressed(Button.A, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
 let list: number[][] = []
 list = [
 [
@@ -57,17 +80,17 @@ list = [
 ]
 ]
 control.inBackground(function () {
-    led2(0, 1000)
-})
-control.inBackground(function () {
     led2(3, 200)
 })
 control.inBackground(function () {
-    led2(1, 500)
+    led2(0, 1000)
 })
 control.inBackground(function () {
     led2(4, 100)
 })
 control.inBackground(function () {
     led2(2, 250)
+})
+control.inBackground(function () {
+    led2(1, 500)
 })
